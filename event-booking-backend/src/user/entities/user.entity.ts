b5b183c563a9f5b5ffd2c2ff713e7962380c6aeb
samira kibous <kiboussamira71@gmail.com>
2026-02-04 09:18:45 +0100

    ARD-34 jwt startegy

--- event-booking-backend/src/user/entities/user.entity.ts
+++ event-booking-backend/src/user/entities/user.entity.ts
@@ -1,8 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 import { Role } from '../../common/enums/role.enum';
 
-export class User {
-  id: number;
-  username: string;
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
   password: string;
+
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  profilePicture: string;
+
+  @Prop({ enum: Role, default: Role.PARTICIPANT })
   role: Role;
+
+  @Prop({ select: false })
+  currentHashedRefreshToken?: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+
+  @Prop({ default: null })
+  deletedAt: Date;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
