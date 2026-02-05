ec0a9dd863194fb85b56086a736c684818f1d88b
samira kibous <kiboussamira71@gmail.com>
2026-02-05 09:49:45 +0100

    ARD-29 shema et dto

--- event-booking-backend/src/app.module.ts
+++ event-booking-backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { EventModule } from './event/event.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
      AuthModule,
-  MongooseModule.forRoot(process.env.MONGODB_URI || '')],
+  MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+  EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ event-booking-backend/src/event/event.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventService } from './event.service';
+import { EventController } from './event.controller';
+import { Event, EventSchema } from './entities/event.entity';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+    AuthModule,
+  ],
+  controllers: [EventController],
+  providers: [EventService],
+  exports: [EventService],
+})
+export class EventModule {}
